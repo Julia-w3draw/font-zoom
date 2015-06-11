3c1f0655f493d841a5c72b490f9c4962f8725a02
Julia <Julia-w3draw@users.noreply.github.com>
2015-06-11 14:37:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,17 @@
 	<body>
 		<div id='_main' style='font-size: 1em'>
 			Fusce vel nisi eu lacus viverra condimentum. Cras sed pharetra tortor, et sollicitudin arcu. Aliquam quam tortor, luctus sed ultrices sed, gravida lobortis leo. Ut ac urna pharetra, scelerisque neque in, blandit risus. Aliquam erat volutpat. Praesent rhoncus at velit id mollis. Donec eget arcu quam. Aliquam ut orci ut turpis sollicitudin faucibus quis at nulla. Morbi ut nibh magna. Aliquam vestibulum neque tempus, ornare quam quis, accumsan ligula. Praesent eu elit sit amet tellus venenatis tempus a quis metus. Proin ac vestibulum libero. Aliquam erat volutpat. Mauris porttitor est a pellentesque posuere. Nunc consequat nunc ut lacus cursus ultrices. Aliquam mattis vel lacus ac lacinia. 
-		</div>
-		<br>
+		</div>		<br>
 		<div id='font_up' onclick='_font_up()' style='width: 50px; height: 30px; background: rgba(0,0,0,0.5)'>F++</div>
 		<br>
 		<div id='font_down' onclick='_font_down()' style='width: 50px; height: 30px; background: rgba(0,0,0,0.5)'>F--</div>
-
 		<script>
-		var _main=document.getElementById('_main')		
-		
+		var _main=document.getElementById('_main')	
 		function _font_up(){			
 			var _font_size=parseFloat(_main.style.fontSize)
 			_font_size+=.1
 			_main.style.fontSize=_font_size+'em'
 		}
-		
 		function _font_down(){
 			var _font_size=parseFloat(_main.style.fontSize)
 			_font_size-=.1
